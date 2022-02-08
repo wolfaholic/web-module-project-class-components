f866498b58d643b146512409495744bb4ca629d6
Melissa <wolfaholic@users.noreply.github.com>
2022-02-07 20:23:51 -0800

    added in initial static dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,21 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+       <h1>To Do App</h1>
+      <div>
+        <ui>
+          <li>Do Homework</li>
+          <li>Walk the Dog</li>
+          <li>Do the Dishes</li>
+          <li>Make Dinner</li>
+        </ui>
+
+        <form>
+          <input />
+          <button>Add ToDo</button>
+        </form>
+        <button>Remove ToDo</button>
+      </div>
       </div>
     )
   }
